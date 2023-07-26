023beac0fd57d2f83ac1648691963405ac9bd89f
Pedro Cattori <pcattori@gmail.com>
2023-07-26 12:52:20 -0400

    4.2: lexemes and tokens

---
+++ src/token.ts
@@ -0,0 +1,57 @@
+export type Location = {
+  source: string
+  offset: number
+  length: number
+}
+
+export function lexeme(loc: Location): string {
+  return loc.source.slice(loc.offset, loc.offset + loc.length)
+}
+
+export type Token = Location & (
+  // single character tokens
+  | { type: 'LEFT_PAREN'}
+  | { type: 'LEFT_PAREN'}
+  | { type: 'LEFT_BRACE' }
+  | { type: 'RIGHT_BRACE' }
+  | { type: 'COMMA' }
+  | { type: 'DOT' }
+  | { type: 'MINUS' }
+  | { type: 'PLUS' }
+  | { type: 'SEMICOLON' }
+  | { type: 'SLASH' }
+  | { type: 'STAR' }
+  // one or two character tokens
+  | { type: 'BANG' }
+  | { type: 'BANG_EQUAL' }
+  | { type: 'EQUAL' }
+  | { type: 'EQUAL_EQUAL' }
+  | { type: 'GREATER' }
+  | { type: 'GREATER_EQUAL' }
+  | { type: 'LESS' }
+  | { type: 'LESS_EQUAL' }
+  // literals
+  | { type: 'STRING', value: string }
+  | { type: 'NUMBER', value: number }
+  // keywords
+  | { type: 'AND' }
+  | { type: 'CLASS' }
+  | { type: 'ELSE' }
+  | { type: 'FALSE' }
+  | { type: 'FUN' }
+  | { type: 'FOR' }
+  | { type: 'IF' }
+  | { type: 'NIL' }
+  | { type: 'OR' }
+  | { type: 'PRINT' }
+  | { type: 'RETURN' }
+  | { type: 'SUPER' }
+  | { type: 'THIS' }
+  | { type: 'TRUE' }
+  | { type: 'VAR' }
+  | { type: 'WHILE' }
+  // identifiers
+  | { type: 'IDENTIFIER' }
+  // special tokens
+  | { type: 'EOF' }
+)
