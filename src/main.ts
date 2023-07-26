77efb1a3a070b80a69010be94118c5cfb3853ab1
Pedro Cattori <pcattori@gmail.com>
2023-07-26 12:20:10 -0400

    4.1: the interpreter framework

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+let { args } = Deno
+
+if (args.length === 0) {
+  repl()
+} else if (args.length === 1) {
+  run(args[0])
+} else {
+  console.log("Usage: twix [script]")
+  Deno.exit(64)
+}
+
+function repl() {
+  while (true) {
+    let line = prompt("> ")
+    if (line === null) break
+    console.log(line)
+  }
+}
+
+async function run(file: string) {
+  let source = await Deno.readTextFile(file)
+  console.log(source)
+}
